f2ba64d0140a24a4f1e480891f8525e04c8bc3f4
Yuliya Kogol <kogol.julia@gmail.com>
2020-05-04 19:20:16 +0300

    fix: adjust search requests

--- movie-search/src/script.js
+++ movie-search/src/script.js
@@ -1,91 +1,77 @@
 const posterNotFound = './img/image-not-found.png';
 let keyWord;
-let pageCount = 1;
-//let isSearch = false;
+let isSearch = false;
+let pageCount = 2;
+let info = document.querySelector('.info');
 
 // let swiper = document.querySelector('.swiper-container').swiper
 
 window.addEventListener('DOMContentLoaded', () => {
-  getMovieInfo('Blade runner', 1);
+  displayMovieInfo('Blade runner', 1);
 });
 
-function createCard(el) {
-  let slide = document.createElement('div');
-  slide.classList.add('swiper-slide');
-  slide.insertAdjacentHTML('afterbegin', `<div class="film-year">${el.Year}</div>`);
-  let poster = el.Poster === 'N/A' ? posterNotFound : el.Poster;
-  slide.insertAdjacentHTML('afterbegin', `<div class="film-poster" style="background-image: url(${poster}); " ></div>`);
-  slide.insertAdjacentHTML('afterbegin', `<a href="https://www.imdb.com/title/${el.imdbID}/" class="film-name" target="_blank">${el.Title}</a>`);
-  return slide;
+function loadTranslation(keyWord) {
+  const urlTranslation = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200322T155651Z.de98a60e6a99185e.089aea4237b51c6db082c966f27a7895cd1e8b44&text=${keyWord}&lang=ru-en`
+  return fetch(urlTranslation)
+    .then(response => response.json())
 }
 
-function getMovieInfo(keyWord, pageCount) {
- swiper.removeAllSlides();
- swiper.update();
- 
+function loadMovie(text, pageCount) {
+  return fetch(`https://www.omdbapi.com/?s=${text}&page=${pageCount}&apikey=ce9895c5`)
+    .then(response => response.json())
+}
+
+function createSlide(movie) {
+  movie.Search.forEach(el => {
+    let slide = document.createElement('div');
+    slide.classList.add('swiper-slide');
+    slide.insertAdjacentHTML('afterbegin', `<div class="film-year">${el.Year}</div>`);
+    let poster = el.Poster === 'N/A' ? posterNotFound : el.Poster;
+    slide.insertAdjacentHTML('afterbegin', `<div class="film-poster" style="background-image: url(${poster}); " ></div>`);
+    slide.insertAdjacentHTML('afterbegin', `<a href="https://www.imdb.com/title/${el.imdbID}/" class="film-name" target="_blank">${el.Title}</a>`);
+       fetch(`https://www.omdbapi.com/?i=${el.imdbID}&apikey=ce9895c5`)
+      .then(response => response.json())
+      .then(movie => {
+        slide.insertAdjacentHTML('beforeend', `<a class="film-rating">${movie.imdbRating}</a>`);
+        swiper.appendSlide(slide);
+        swiper.update();
+        document.querySelector('.loader').style.display = 'none';
+      })
+  })
+}
+
+function displayMovieInfo(keyWord, pageCount) {
+  swiper.removeAllSlides();
+  swiper.update();
   document.querySelector('.loader').style.display = 'block';
   let engNum = /[a-zA-Z0-9]/;
-  if (!engNum.test(keyWord)) {
-    const urlTr = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200425T122428Z.8ca2d87f5b800f7e.9a40ebb21a85ec1dd1d616f3bb88861b71b6b3b7&text= ${keyWord} &lang=ru-en`;
-    fetch(urlTr)
-      .then(res => res.json())
-      .then(data => {
-       let translated = data.text.toString().trim();
-       const urlMovie = `https://www.omdbapi.com/?s=${translated}&page=${pageCount}&apikey=ce9895c5`;
-    fetch(urlMovie)
-      .then(respons => respons.json())
-      .then(dat => {
-        console.log(dat)
-        dat.Search.forEach((el) => {
-          let slideA = createCard(el);
-          const urlRating = `https://www.omdbapi.com/?i=${el.imdbID}&apikey=ce9895c5`;
-          fetch(urlRating)
-            .then(res => res.json())
-            .then(result => {
-              slideA.insertAdjacentHTML('beforeend', `<a class="film-rating">${result.imdbRating}</a>`);
-              swiper.appendSlide(slideA);
-              swiper.update();
-            })
-        })
-      })
-        document.querySelector('.loader').style.display = 'none';
-      })
+  if (engNum.test(keyWord)) {
+    loadMovie(keyWord, pageCount)
+      .then(createSlide)
+      .catch(err => 
+    info.innerHTML = `No results for <strong>${keyWord}</strong>`)
   } else {
-    const urlMovie = `https://www.omdbapi.com/?s=${keyWord}&page=${pageCount}&apikey=af56d13e`;
-    fetch(urlMovie)
-      .then(respons => respons.json())
-      .then(dat => {
-        console.log(dat)
-        dat.Search.forEach((el) => {
-          let slideA = createCard(el);
-          const urlRating = `https://www.omdbapi.com/?i=${el.imdbID}&apikey=af56d13e`;
-          fetch(urlRating)
-            .then(res => res.json())
-            .then(result => {
-              slideA.insertAdjacentHTML('beforeend', `<a class="film-rating">${result.imdbRating}</a>`);
-              swiper.appendSlide(slideA);
-              swiper.update();
-            //  swiper.slideTo(0, 400, false);
-            })
-        })
-      })
-        document.querySelector('.loader').style.display = 'none';
+    info.innerHTML = `Showing results for <strong>${keyWord}</strong>`;
+    loadTranslation(keyWord)
+      .then(translation => loadMovie(translation.text, pageCount))
+      .then(createSlide)
+      .catch(err => info.innerHTML = `No results for <strong>${keyWord}</strong>`)
   }
 }
 
 // form submit
 document.querySelector('.search').addEventListener('submit', (e) => {
   e.preventDefault();
+  info.innerHTML = '';
   keyWord = document.querySelector('.search-input').value;
- 
-  getMovieInfo(keyWord, 1);
- 
+  isSearch = true;
+  displayMovieInfo(keyWord, 1);
 })
 
 let swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   spaceBetween: 10,
- // initialSlide: 0,
+  // initialSlide: 0,
   breakpoints: {
     320: {
       slidesPerView: 2,
@@ -107,10 +93,14 @@ let swiper = new Swiper('.swiper-container', {
 });
 
 swiper.on('slideChange', function () {
+  if (isSearch === true) {
+    pageCount = 2;
+  }
   if (swiper.activeIndex === 7) {
-  keyWord = document.querySelector('.search-input').value;
-  getMovieInfo(keyWord, pageCount);
-  pageCount++;
+    isSearch = false;
+    keyWord = document.querySelector('.search-input').value;
+    displayMovieInfo(keyWord, pageCount);
+    pageCount++;
   }
 });
 
