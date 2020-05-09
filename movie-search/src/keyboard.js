b36697ad4d5c3123b88c8eb93f5f34b06c113f3f
Yuliya Kogol <kogol.julia@gmail.com>
2020-05-09 18:46:28 +0300

    feat: add a virtual key for changing language

--- movie-search/src/keyboard.js
+++ movie-search/src/keyboard.js
@@ -1,86 +1,36 @@
 const charsLatin = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
   'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', ']', '[', '\\', 'Del',
-  'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter', 'Shift',
-  'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', 'Shift', 'Ctrl', 'Alt', ' ', 'Alt', '←', '↓', '→', 'Ctrl'];
+  'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter',
+  'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', 'Lang', ' ', '←', '↓', '→'];
 const charsCyrillic = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
   'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del',
-  'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift',
-  'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '↑', 'Shift', 'Ctrl', 'Alt', ' ', 'Alt', '←', '↓', '→', 'Ctrl'];
+  'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter',
+  'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '↑', 'Lang', ' ', '←', '↓', '→'];
 const keyCodes = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace',
   'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete',
-  'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft',
-  'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
-const shiftLatin = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace',
-  'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'Del',
-  'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '\'', 'Enter', 'Shift',
-  'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '↑', 'Shift', 'Ctrl', 'Alt', ' ', 'Alt', '←', '↓', '→', 'Ctrl'];
-const shiftCyrillic = ['Ё', '!', '\'', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'Backspace',
-  'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/', 'Del',
-  'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter', 'Shift',
-  'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', '↑', 'Shift', 'Ctrl', 'Alt', ' ', 'Alt', '←', '↓', '→', 'Ctrl'];
+  'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter',
+  'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'Space', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
 const input = document.querySelector('.search-input');
 const keyboardWindow = document.querySelector('.keyboard-window');
 const keysContainer = [];
 let key;
 
-class Helper {
-  constructor() {
-    this.isAlt = false;
-    this.isControl = false;
-  }
-
-  toggleCapsLock() {
-    this.caps = !this.caps;
-    document.querySelectorAll('button').forEach((item) => {
-      if (item.textContent.length === 1) {
-        item.textContent = this.caps ? item.textContent.toUpperCase()
-          : item.textContent.toLowerCase();
-      }
-    });
-  }
-
-  changeLang() {
-    if (this.isEnglish === true) {
-      keysContainer.forEach((item, idx) => {
-        item.textContent = charsCyrillic[idx];
-      });
-      this.isEnglish = false;
-      localStorage.setItem('isEnglish', false);
-    } else {
-      keysContainer.forEach((item, idx) => {
-        item.textContent = charsLatin[idx];
-      });
-      this.isEnglish = true;
-      localStorage.setItem('isEnglish', true);
+function toggleCapsLock() {
+  this.caps = !this.caps;
+  document.querySelectorAll('button').forEach((item) => {
+    if (item.textContent.length === 1) {
+      item.textContent = this.caps ? item.textContent.toUpperCase()
+        : item.textContent.toLowerCase();
     }
-  }
-
-  onShiftPressed() {
-    if (this.isEnglish === true) {
-      keysContainer.forEach((item, idx) => {
-        item.textContent = shiftLatin[idx];
-      });
-    } else {
-      keysContainer.forEach((item, idx) => {
-        item.textContent = shiftCyrillic[idx];
-      });
-    }
-  }
-
-  onShiftReleased() {
-    if (this.isEnglish === true) {
-      keysContainer.forEach((item, idx) => {
-        item.textContent = charsLatin[idx];
-      });
-    } else {
-      keysContainer.forEach((item, idx) => {
-        item.textContent = charsCyrillic[idx];
-      });
-    }
-  }
+  });
 }
 
-const helper = new Helper();
+function toggleLang() {
+  this.language = !this.language;
+  keysContainer.forEach((item, idx) => {
+    item.textContent = this.language ? charsLatin[idx] : charsCyrillic[idx];
+  });
+}
 
 // key handlers
 function handleSymbols() {
@@ -126,13 +76,7 @@ function handleDelete(item) {
 }
 
 function createKeys() {
-  let lang;
-  if (localStorage.getItem('isEnglish') === true || typeof localStorage.getItem('isEnglish') === 'undefined') {
-    lang = charsLatin;
-  } else {
-    lang = charsCyrillic;
-  }
-  lang.forEach((item, idx) => {
+  charsCyrillic.forEach((item, idx) => {
     key = document.createElement('button');
     key.setAttribute('type', 'button');
     key.setAttribute('id', `${keyCodes[idx]}`);
@@ -151,19 +95,9 @@ function createKeys() {
       case 'Enter':
         key.classList.add('keyboard__key-wide');
         break;
-      case 'Tab':
-        key.addEventListener('mousedown', () => {
-          input.value += '    ';
-        });
-        break;
-      case 'Shift':
-        key.classList.add('keyboard__key-wide');
-        key.addEventListener('mousedown', helper.onShiftPressed);
-        key.addEventListener('mouseup', helper.onShiftReleased);
-        break;
       case 'CapsLock':
         key.classList.add('keyboard__key-wide');
-        key.addEventListener('mousedown', helper.toggleCapsLock);
+        key.addEventListener('mousedown', toggleCapsLock);
         break;
       case '←':
         key.addEventListener('mousedown', () => {
@@ -176,6 +110,10 @@ function createKeys() {
           input.selectionStart += 1;
           input.selectionEnd += 1;
         });
+        break;
+      case 'Lang':
+        key.classList.add('keyboard__key-wide');
+        key.addEventListener('mousedown', toggleLang);
         break;
       default:
     }
@@ -192,12 +130,12 @@ function createKeyboard() {
   keyboard.append(container);
   createKeys().forEach((item) => {
     container.append(item);
-    if (item.id === 'Backspace' || item.id === 'Delete' || item.id === 'Enter' || item.id === 'ShiftRight') {
+    if (item.id === 'Backspace' || item.id === 'Delete' || item.id === 'Enter' || item.id === '↑') {
       const br = document.createElement('br');
       container.append(br);
     }
   });
-  keyboardWindow.innerHTML = 'Change language: Ctrl + Alt<br>';
+  keyboardWindow.innerHTML = 'Change language: Lang virtual key<br>';
   keyboardWindow.insertAdjacentHTML('afterbegin', '<div class="keyboard-window--close">×</div>');
   keyboardWindow.append(keyboard);
 
@@ -205,34 +143,6 @@ function createKeyboard() {
     keyboardWindow.classList.add('hidden');
   });
 }
-
-document.addEventListener('keydown', (event) => {
-  switch (event.key) {
-    case 'Alt':
-      helper.isAlt = true;
-      event.preventDefault();
-      break;
-    case 'Control':
-      helper.isControl = true;
-      break;
-    default:
-  }
-  if (helper.isControl === true && helper.isAlt === true) {
-    helper.changeLang();
-  }
-});
-
-document.addEventListener('keyup', (event) => {
-  switch (event.key) {
-    case 'Control':
-      helper.isControl = false;
-      break;
-    case 'Alt':
-      helper.isAlt = false;
-      break;
-    default:
-  }
-});
 
 document.querySelector('.search-keyboard').addEventListener('click', () => {
   keyboardWindow.classList.remove('hidden');
